174eeef35b6e680b147c30be74746fdf54f67be1
kiwenlau <kiwenlau@126.com>
2015-07-10 16:35:32 +0900

    add comments

--- copylog.py
+++ copylog.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
 
+# copy logs of all benchmarks 
+# input: the directory of benchmark result
+# logs: save all logs
+
 import os, subprocess
 
+
+# get the path of all logs
 p = os.popen("find input -type d -name logs")
-
 lines = p.readlines()
-
 p.close()
-#print lines
 
+# create the directory for saving all logs
 if os.path.exists("logs"):
     shutil.rmtree("logs")
-
 os.mkdir("logs")
 
+# copy all logs from benchmark result
 i=1
-
 for line in lines :
     source=line.rstrip()
     destination="logs/logs"+str(i)
     subprocess.check_output(["cp", "-r", source, destination])
     i=i+1
-    #print destination
-
-     
-
-
-
-
